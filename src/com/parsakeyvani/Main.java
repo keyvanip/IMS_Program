a90eec0af0cbc49838e1bdd471bf9010b9ea429e
Ian Sulley <57299945+sulleyi@users.noreply.github.com>
2021-03-14 22:42:32 -0400

    Update Main.java

--- src/com/parsakeyvani/Main.java
+++ src/com/parsakeyvani/Main.java
@@ -1,6 +1,14 @@
 package com.parsakeyvani;
 
-public class Main {
+/*
+“As a student at Union College, I am part of a community that values intellectual effort, curiosity and discovery. I understand that in order to truly claim my educational and academic achievements, I am obligated to act with academic integrity. Therefore, I affirm that I will carry out my academic endeavors with full academic honesty, and I rely on my fellow students to do the same.”
+
+Signed, 
+Blake Luther, Ian Sulley, Parsa Keyvani
+3/14/21
+
+*/
+class Main {
 
     public static void main(String[] args) {
 	Products myProducts= new Products("Pencil", 2.33, 23);
